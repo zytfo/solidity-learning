e8961540224d0b83a2e5c9e25499012474940fe3
Artur <khayalievartur@gmail.com>
2024-01-02 08:10:25 +0100

    add remix contracts

---
+++ remix-contracts/storage/StorageFactory.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    // address[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _newSimpleStorageNumber
+    ) public {
+        // address
+        // ABI
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_newSimpleStorageNumber];
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(
+            _newSimpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+}
