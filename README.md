83415cefcf262c8361f1fda0af624509ca6e2d56
Artur <khayalievartur@gmail.com>
2024-01-06 01:11:01 +0100

    add foundry-fund-me-f23

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Foundry (foundry-f23)
 
-0. `forge --init` - create a new project
+0. `forge init` - create a new project
 1. `forge compile` - compile contract
 2. `forge create SimpleStorage  --rpc-url http://127.0.0.1:7545 --interactive` - via Ganache
 3. `anvil; forge create SimpleStorage --interactive` - via Anvil (anvil in another tab)
@@ -13,3 +13,12 @@
 8. `cast send 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "store(uint256)" 123 --rpc-url $RPC_URL --private-key $PRIVATE_KEY` - send method on deployed contract from terminal
 9. `cast call 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "retrieve()"` - call method from terminal
 10. `forge fmt` - format all solidity code
+
+### foundry-fund-me-f23
+
+1. `forge init` - init a new project
+2. `forge test` - run test
+3. `forge install smartcontractkit/chainlink-brownie-contracts --no-commit` - install chainlink libs
+4. Add into `foundry.toml`: `remappings = ["@chainlink/contracts/=lib/chainlink-brownie-contracts/contracts/",]`
+5. `forge remappings > remappings.txt` - add remappings for libs
+6. `forge test -vv` - run tests with console.log
---
+++ foundry-f23/foundry-fund-me-f23/src/FundMe.sol
@@ -0,0 +1,121 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+
+// 809,158
+// 789,428
+contract FundMe {
+    using PriceConverter for uint256;
+    address public immutable i_owner;
+
+    uint256 public constant MINIMUM_USD = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "must be owner");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "didn't send enough eth"
+        );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // transfer
+        // msg.sender = address
+        // payable(msg.sender) = payable address;
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSucess = payable(msg.sender).send(address(this).balance);
+        // require(sendSucess, "send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    // receive() - calls when just send eth to contract
+    // fallback() - calls when there is some calldata
+
+    /*
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    // uint256 public minimumFundValue;
+    // address _owner;
+    // mapping(address => uint256) userToFunds;
+
+    // constructor() {
+    //     minimumFundValue = 1;
+    //     _owner = msg.sender;
+    // }
+
+    // modifier isOwner(address _sender) {
+    //     require(_sender == _owner);
+    //     _;
+    // }
+
+    // function setMinimumFundValue(uint256 _amount) public isOwner(msg.sender) {
+    //     minimumFundValue = _amount;
+    // }
+
+    // function getFundByUser(address _userAddress) public view returns (uint256) {
+    //     return userToFunds[_userAddress];
+    // }
+}
